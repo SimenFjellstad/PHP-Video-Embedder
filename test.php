efea84d587767966de79462b6f6e794f54630ef5
Simen Fjellstad <zaimennor@gmail.com>
2015-04-06 05:00:05 +0200

    Added hitbox support
    Added hitbox support

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
 /*
-	This is a test file, it's here to show you how to use this plugin.
+	This is a test project, to show you how this works.
 */
+	$link = "http://www.hitbox.tv/combo2k";
 	include('embedder.php');
-	setDebug(false);
-	embed_video('https://www.youtube.com/watch?v=WsSQL1hIiro');
+	setDebug(true);
+	embed_video($link);
 	echo '<br><br>';
-	embed_sized_video('https://www.youtube.com/watch?v=WsSQL1hIiro',720,480);
+	embed_sized_video($link,720,480);
 ?>	
